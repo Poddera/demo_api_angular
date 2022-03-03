666e41ca8eab3d9e663e8caff528bd8b8bf1abf3
CrisDgrnu <cris@pop-os.localdomain>
2022-03-03 09:19:22 +0100

    Add login component documentation and update suscribe practice

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { LoginService } from '../../shared/login.service';
 
 @Component({
@@ -19,33 +18,49 @@ export class LoginComponent implements OnInit {
     private loginService: LoginService,
     private router: Router
   ) {
-    this.loginForm = new FormGroup({
-      username: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required]),
-    });
+    this.loginForm = this.initForm();
   }
 
   ngOnInit(): void {}
 
+  /**
+   * Navigate to the user home page or display an error
+   * @author cristian 20220303
+   */
   onSubmit() {
     if (this.loginForm.valid) {
-      this.loginService.login(this.loginForm.value).subscribe(
-        (username: string) => {
+      this.loginService.login(this.loginForm.value).subscribe({
+        next: (username: string) => {
           this.router.navigate(['home', username]);
         },
-        (error: HttpErrorResponse) => {
+        error: (error: HttpErrorResponse) => {
           console.error(error);
-          this.snackBar.open(
-            'The providen credentials are not correct',
-            'Close'
-          );
-        }
-      );
+          this.showErrorMessage('The providen credentials are not correct');
+        },
+      });
     } else {
-      this.snackBar.open(
-        'Please ensure that all the fields are filled',
-        'Close'
-      );
+      this.showErrorMessage('Please ensure that all the fields are filled');
     }
+  }
+
+  /**
+   * Displays a snackBar with a custom message
+   * @author cristian 20220303
+   * @param message The message to display
+   */
+  private showErrorMessage(message: string) {
+    this.snackBar.open(message, 'Close');
+  }
+
+  /**
+   * Initializes the form to login
+   * @author cristian 20220303
+   * @returns The login form initialized
+   */
+  private initForm(): FormGroup {
+    return new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
   }
 }
