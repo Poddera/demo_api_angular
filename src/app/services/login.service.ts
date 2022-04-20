59b9401805bdb1b9192b1549c0b6f0c183dfdad4
cfres <cris.dgrnu@gmail.com>
2022-04-20 18:17:42 +0200

    Fix login to work with new endpoint

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,60 +1,52 @@
 import {
-    HttpClient,
-    HttpErrorResponse,
-    HttpResponse,
-} from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { catchError, map, Observable, throwError } from 'rxjs';
-import { URLS } from 'src/environments/environment';
-import { LoginDTO } from '../components/auth/components/login/dto';
+  HttpClient,
+  HttpErrorResponse,
+  HttpResponse,
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, map, tap, Observable, throwError } from "rxjs";
+import { URLS } from "src/environments/environment";
+import { LoginDTO } from "../components/auth/components/login/dto";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class LoginService {
-    loggedIn: boolean;
+  loggedIn: boolean;
 
-    constructor(private httpClient: HttpClient) {
-        this.loggedIn = false;
-    }
+  constructor(private httpClient: HttpClient) {
+    this.loggedIn = false;
+  }
 
-    login(loginDTO: LoginDTO): Observable<string> {
-        return this.httpClient
-            .post(`${URLS.BASE_URL_JWT}/api/portal/v1`, loginDTO, {
-                observe: 'response',
-            })
-            .pipe(
-                map((res: HttpResponse<any>) => {
-                    localStorage.setItem(
-                        'access_token',
-                        res.body.access_token
-                    );
-                    this.loggedIn = true;
-                    return res.body.user;
-                }),
-                catchError(this.handleError)
-            );
-    }
+  login(loginDTO: LoginDTO): Observable<Object> {
+    return this.httpClient
+      .post(`${URLS.BASE_URL_JWT}/rest/login`, loginDTO, {
+        observe: "response",
+        responseType: "text",
+      })
+      .pipe(
+        tap(() => {
+          this.loggedIn = true;
+        }),
+        catchError(this.handleError),
+      );
+  }
 
-    logout() {
-        localStorage.removeItem('access_token');
-        this.loggedIn = false;
-    }
+  logout() {
+    localStorage.removeItem("access_token");
+    this.loggedIn = false;
+  }
 
-    private handleError(error: HttpErrorResponse) {
-        if (error.status === 0)
-            return throwError(
-                () => new Error(`An error occurred: ${error.error}`)
-            );
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0)
+      return throwError(() => new Error(`An error occurred: ${error.error}`));
 
-        if (error.status === 401)
-            return throwError(
-                () => new Error(`Unauthorized: ${error.error}`)
-            );
+    if (error.status === 401)
+      return throwError(() => new Error(`Unauthorized: ${error.error}`));
 
-        return throwError(
-            () =>
-                new Error(
-                    `Backend returned code ${error.status}, body was: ${error.error} `
-                )
-        );
-    }
+    return throwError(
+      () =>
+        new Error(
+          `Backend returned code ${error.status}, body was: ${error.error} `,
+        ),
+    );
+  }
 }
